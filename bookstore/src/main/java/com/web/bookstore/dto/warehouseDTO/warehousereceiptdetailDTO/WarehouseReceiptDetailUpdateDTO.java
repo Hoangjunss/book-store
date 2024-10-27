bab178eda5adac6f13601a2fac42a2642d7d5895
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2024-10-27 09:26:56 +0700

    edit WarehouseReceiptDetailUpdateDTO

--- bookstore/src/main/java/com/web/bookstore/dto/warehouseDTO/warehousereceiptdetailDTO/WarehouseReceiptDetailUpdateDTO.java
+++ bookstore/src/main/java/com/web/bookstore/dto/warehouseDTO/warehousereceiptdetailDTO/WarehouseReceiptDetailUpdateDTO.java
@@ -9,8 +9,8 @@ import java.math.BigDecimal;
 
 @Data
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class WarehouseReceiptDetailUpdateDTO {
 
     private Integer id;
