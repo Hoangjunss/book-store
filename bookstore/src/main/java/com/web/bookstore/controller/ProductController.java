74dcf14b3188f1de4201e9fc045b73489d9b83b2
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-10-31 22:02:43 +0700

    add

--- bookstore/src/main/java/com/web/bookstore/controller/ProductController.java
+++ bookstore/src/main/java/com/web/bookstore/controller/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -31,6 +32,7 @@ public class ProductController {
         productService.deleteProduct(id);
         return  ResponseEntity.ok("delete success full");
     }
+    @PreAuthorize("permitAll()")
     @GetMapping
     public ResponseEntity<Page<ProductDTO>> getAllProducts(
             @RequestParam(defaultValue = "0") int page,
