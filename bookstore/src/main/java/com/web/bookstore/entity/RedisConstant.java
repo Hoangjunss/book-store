e3febda34f27c7e8f495d42c125af613ef04ee81
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-11-04 20:30:22 +0700

    add redis user

--- bookstore/src/main/java/com/web/bookstore/entity/RedisConstant.java
+++ bookstore/src/main/java/com/web/bookstore/entity/RedisConstant.java
@@ -3,10 +3,11 @@ package com.web.bookstore.entity;
 public class RedisConstant {
     public static final String LIST_PRODUCT_All = "listProductAll";
     public static final String LIST_PRODUCT_All_SUPPLY = "listProductAllSupply";
+    public static final String PRODUCT = "product";
     public static final String LIST_PRODUCT_STATUS = "listProductStatus";
     public static final String LIST_CATEGORY = "listCategory";
 
-    public static final String PRODUCT = "product";
+
     public static final String CATEGORY = "category";
     public static final String CART = "cart";
     public static final String CART_DETAIL = "cartDetail";
@@ -17,6 +18,7 @@ public class RedisConstant {
     public static final String ORDER_DETAIL = "orderDetail";
     public static final String USER_ID = "userId";
     public static final String USER_EMAIL = "userEmail";
+    public static final String USER_ROLE = "userRole";
 
 
     public static final String LIST_USER = "listUser";
