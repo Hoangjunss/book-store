4c5cef2e3a12e424ab65f2643cb468be22e0e802
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2024-12-05 09:36:44 +0700

    add

--- bookstore/src/main/java/com/web/bookstore/dto/orderDTO/ordersDTO/OrdersCreateDTO.java
+++ bookstore/src/main/java/com/web/bookstore/dto/orderDTO/ordersDTO/OrdersCreateDTO.java
@@ -1,6 +1,7 @@
 package com.web.bookstore.dto.orderDTO.ordersDTO;
 
 import com.web.bookstore.dto.orderDTO.orderdetailDTO.OrderDetailCreateDTO;
+import com.web.bookstore.dto.otherDTO.addressDTO.AddressCreateDTO;
 import com.web.bookstore.dto.otherDTO.addressDTO.AddressDTO;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,7 +18,7 @@ import java.util.List;
 public class OrdersCreateDTO {
     private Integer quantity;
     private BigDecimal totalPrice;
-    private AddressDTO address;
+    private AddressCreateDTO address;
     private String paymentStatus;
     private String orderStatus;
 }
